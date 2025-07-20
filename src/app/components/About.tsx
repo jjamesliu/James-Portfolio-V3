3304ae6e0da214894e33d067fd7fac1ca516c6fc
jjamesliu <liujameszhi@gmail.com>
2025-07-20 14:15:27 -0700

    finished about section

---
+++ src/app/components/About.tsx
@@ -0,0 +1,15 @@
+export default function About() {
+    return (
+        <div className='flex flex-col gap-4 border border-gray-300 border-t-0 py-6 px-15'>
+
+                <h1 className='font-semibold font-mono'>ABOUT</h1>
+                <p className='text-txt-color'>
+                    I'm a Full Stack Developer focused on creating user-friendly impactful
+                    web applications. I design responsive UIs, developing web applications,
+                    from concept to deployment. Currently studying at University of California:
+                    Los Angeles
+                </p>
+
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import Profile from "./components/Profile";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <div className='font-sans max-w-4xl mx-auto'>
       <Header />
       <Profile />
+      <About />
     </div>
 
   );
