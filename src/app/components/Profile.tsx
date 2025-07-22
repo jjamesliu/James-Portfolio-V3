448dcddb7fbd1203cb76aff196e6bfcec03f0096
jjamesliu <liujameszhi@gmail.com>
2025-07-21 17:51:07 -0700

    added reusme

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { FileUser } from 'lucide-react';
+
 
 export default function Profile() {
     return (
@@ -13,6 +15,12 @@ export default function Profile() {
             <div className='flex flex-col items-center mt-4 gap-1'>
                 <h1 className='text-3xl font-bold'>James Liu</h1>
                 <h3 className='text-xl font-medium text-txt-color'>Full Stack Developer</h3>
+                <div className='rounded-lg  border-gray-500 border-2 text-gray-700 shadow-lg px-1.5 py-1 mt-2 hover:scale-110 duration-300 hover:bg-green-400 hover:text-gray-700'>
+                    <a className='flex items-center justify-center gap-1 font-semibold text-[0.9rem]'
+                    href="/james_resume.pdf" target="_blank">
+                        <FileUser className='w-6'/>Resume 
+                    </a>
+                </div>
             </div>
         </div>
     )
