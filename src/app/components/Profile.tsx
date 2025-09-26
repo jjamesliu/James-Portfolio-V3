224b106026d603744787d3205be44f65da490f8a
jjamesliu <liujameszhi@gmail.com>
2025-09-25 23:14:22 -0700

    typing animation

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,5 +1,7 @@
+"use client"
 import Image from "next/image";
 import { FileUser, MoveRight } from 'lucide-react';
+import { TypeAnimation } from "react-type-animation";
 
 
 export default function Profile() {
@@ -7,7 +9,7 @@ export default function Profile() {
         <div className='pt-4 pb-8 mx-auto border border-gray-400 border-dotted border-t-0'>
             <a href="https://zhidesign.framer.website/" target="_blank" rel="noopener noreferrer">
                 <div className='flex justify-end'>
-                    <div className='inline-flex flex-row gap-1 text-txt-color text-xs items-center pr-2 mb-2 hover:scale-105 hover:text-black transform transition duration-300 fade cursor-pointer'>
+                    <div className='inline-flex flex-row gap-1 text-txt-color text-xs items-center pr-2 mt-2 mb-8 hover:scale-105 hover:text-black transform transition duration-300 fade cursor-pointer'>
                         <MoveRight size={12}/>
                         <span>ecommerce/agency portfolio</span>
                     </div>
@@ -22,8 +24,23 @@ export default function Profile() {
                 />
             </div>
             <div className='flex flex-col items-center mt-4 gap-1'>
-                <h1 className='text-3xl font-bold'>James Liu</h1>
-                <h3 className='text-xl font-medium text-txt-color'>Full Stack Developer</h3>
+                <h1 className='text-3xl font-bold'>
+                    <TypeAnimation
+                    sequence={['James Liu'
+                    ]}
+                    wrapper="span"
+                    speed={3}
+                    cursor={false}
+                    repeat={0}/>
+                </h1>
+                <h3 className='text-xl font-medium text-txt-color'>
+                    <TypeAnimation
+                    sequence={['Full Stack Developer']}
+                    wrapper="span"
+                    speed={5}
+                    cursor={false}
+                    repeat={0}/>
+                </h3>
                 <div className='rounded-lg  border-black border-2 text-gray-700 shadow-lg px-1.5 py-1 mt-2 hover:scale-110 duration-300 hover:bg-green-400 hover:text-gray-700 active:bg-green-400 active:text-gray-700'>
                     <a className='flex items-center justify-center gap-1 font-semibold text-[0.9rem]'
                     href="/james_resume.pdf" target="_blank">
